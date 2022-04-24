e5472f180966f9046b1276f74eabd288e1fcebc0
Gabriel Pizzo <36797166+gabriel04100@users.noreply.github.com>
2022-04-24 22:42:19 +0200

    Update predict-sales.py

--- sales/predict-sales.py
+++ sales/predict-sales.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-import os
 from sklearn.ensemble import IsolationForest
 import statsmodels.api as sm
 
